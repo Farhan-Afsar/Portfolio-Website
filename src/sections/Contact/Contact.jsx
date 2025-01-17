6e87739984dc431819e75c27fd0b91ff524bfbd5
Farhan Afsar <mdfarhanafsar@gmail.com>
2025-01-18 04:52:51 +0600

    Fixed Form Data Reset

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,11 @@ function Contact() {
   return (
    <section id = "contact" className={styles.container}>
     <h1 className='sectionTitle'>Contact</h1>
-    <form action="https://formspree.io/f/meoowdjo" method='post'>
+    <form action="https://formspree.io/f/meoowdjo" method='post'
+     onSubmit={(e) => {
+        e.target.reset(); 
+      }}
+    >
         <div className='formGroup'>
             <label htmlFor="name" hidden>
                 Name
